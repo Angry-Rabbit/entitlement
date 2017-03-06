511dcbc47e189d63bf1f2bcba812c8f5fe8714a9
HeFan <victorxfan@163.com>
2017-03-06 15:52:56 +0800

    删除公司标识

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,6 +5,6 @@ module.exports = {
   name: 'Entitlement System',
   logoSrc: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
   logoText: '权管系统(ES)',
-  copyright: '版权所有 © 2017 深圳市凯如科技有限公司',
+  copyright: '版权所有 © 2017 xx公司',
   PAGE_SIZE: 10,
 }
